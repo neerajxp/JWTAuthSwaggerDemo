a2ea0c917b66a88fd0c9dc098478de8b5111c1a2
neerajxp <neerajxp@gmail.com>
2020-08-30 15:20:38 -0400

    JWT Token and Swagger Integration in .net Core
    JWT Token and Swagger Integration in .net Core

---
+++ JWTAuthSwaggerDemo/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [Authorize]
+        [HttpPost]
+        public string GetProducts()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Welcomm To:" + userName;
+        }
+
+        [Authorize]
+        [HttpGet("GetAll")]
+        public ActionResult<IEnumerable<string>> GetAllValues()
+        {
+            return new string[] { "Value1", "Value2", "Value3" };
+        }
+
+        [Authorize]
+        [HttpGet("GetOne")]
+        public ActionResult<IEnumerable<string>> GetOneValue(int i)
+        {
+            return new string[] { "Value" + i };
+        }
+    }
+}
---
+++ JWTAuthSwaggerDemo/Controllers/TokenController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTAuthDemo.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public IActionResult Token(string username, string pass)
+        {
+            UserModel login = new UserModel();
+            login.UserName = username;
+            login.Password = pass;
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+            if(user!=null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+                return response;
+            }
+            else
+            {
+                return BadRequest();
+            }            
+        }
+       
+        private string GenerateJSONWebToken(UserModel userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userinfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, userinfo.EmailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token= new JwtSecurityToken(
+                issuer:_config["Jwt:Issuer"],
+                audience:_config["Jwt:Issuer"],
+                claims,
+                expires:DateTime.Now.AddMinutes(120),
+                signingCredentials:credentials);
+
+            var encodertoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodertoken;
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if (login.UserName == "neerajxp" && login.Password == "123")
+            {
+                user = new UserModel { UserName = "neerajxp", EmailAddress = "neerajxp@gmail.com", 
+                    Password = "123" };
+            }
+            return user;
+        }
+    }
+}
